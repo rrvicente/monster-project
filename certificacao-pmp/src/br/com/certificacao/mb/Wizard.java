5dce70ff5e5c21226694f28296f203924c599b53
Rafael Rodrigo Vicente <bnurafa@gmail.com>
2015-07-08 20:14:46 -0300

    Ajustes gerais

--- certificacao-pmp/src/br/com/certificacao/mb/Wizard.java
+++ certificacao-pmp/src/br/com/certificacao/mb/Wizard.java
@@ -5,11 +5,15 @@ import java.io.Serializable;
 import javax.enterprise.context.Conversation;
 import javax.enterprise.context.ConversationScoped;
 import javax.enterprise.inject.Produces;
+import javax.faces.component.html.HtmlSelectOneRadio;
 import javax.faces.event.ActionEvent;
+import javax.faces.event.AjaxBehaviorEvent;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import br.com.certificacao.entidades.Resposta;
 import br.com.certificacao.entidades.dao.PerguntaDao;
+import br.com.certificacao.entidades.dao.RespostaDao;
 import br.com.certificacao.quiz.Question;
 import br.com.certificacao.quiz.Questions;
 
@@ -19,12 +23,24 @@ public class Wizard implements Serializable {
 	private static final long serialVersionUID = 1L;
 	private Questions questions;
 	private int cursor = 0;
+	private int time = 30;
+
+	public int getTime() {
+		return time;
+	}
+
+	public void setTime(int time) {
+		this.time = time;
+	}
 
 	@Inject
 	private Conversation conversation;
 
 	@Inject
 	private PerguntaDao perguntaDao;
+
+	@Inject
+	private RespostaDao respostaDao;
 
 	@Produces
 	@Named
@@ -37,6 +53,7 @@ public class Wizard implements Serializable {
 	}
 
 	public void nextButtonClicked(ActionEvent e) {
+		questions.getQuestions()[cursor].setAnswered(true);
 		incrementCursor();
 	}
 
@@ -61,8 +78,7 @@ public class Wizard implements Serializable {
 	}
 
 	public boolean getNextButtonEnabled() {
-		return cursor != questions.size() - 1
-				&& (questions.getQuestions())[cursor].isAnswered();
+		return cursor != questions.size() - 1 && (questions.getQuestions())[cursor].isAnswered();
 	}
 
 	public boolean getPreviousButtonEnabled() {
@@ -70,8 +86,7 @@ public class Wizard implements Serializable {
 	}
 
 	public boolean getFinishButtonEnabled() {
-		return cursor == questions.size() - 1
-				&& (questions.getQuestions())[cursor].isAnswered();
+		return cursor == questions.size() - 1 && (questions.getQuestions())[cursor].isAnswered();
 	}
 
 	public String start() {
@@ -81,11 +96,22 @@ public class Wizard implements Serializable {
 
 	public String end() {
 		conversation.end();
-		return "/done";
+		return "/finish";
 	}
 
 	private void setCurrentQuestionUnanswered() {
 		Question currentQuestion = (questions.getQuestions())[cursor];
 		currentQuestion.setAnswered(false);
+	}
+
+	public void valueChange(AjaxBehaviorEvent evt) {
+		HtmlSelectOneRadio o = ((HtmlSelectOneRadio) evt.getSource());
+		String id = (String) o.getSubmittedValue();
+
+		Resposta resposta = respostaDao.pesquisarPorId(Integer.parseInt(id));
+
+		if (resposta != null) {
+			(questions.getQuestions())[cursor].setAnswer(resposta);
+		}
 	}
 }
--- certificacao-pmp/src/br/com/certificacao/quiz/Question.java
+++ certificacao-pmp/src/br/com/certificacao/quiz/Question.java
@@ -9,14 +9,7 @@ public class Question implements Serializable {
 	private static final long serialVersionUID = 1284490087332362658L;
 	private Pergunta question;
 	private Resposta answer;
-	// private String question, answer;
-	// private String[] choices;
 	private boolean answered = false;
-
-	// public Question(String question, String[] choices) {
-	// this.question = question;
-	// this.choices = choices;
-	// }
 
 	public Question(Pergunta question) {
 		this.question = question;
@@ -34,10 +27,6 @@ public class Question implements Serializable {
 	public Pergunta getQuestion() {
 		return question;
 	}
-
-	// public String[] getChoices() {
-	// return choices;
-	// }
 
 	public boolean isAnswered() {
 		return answered;
